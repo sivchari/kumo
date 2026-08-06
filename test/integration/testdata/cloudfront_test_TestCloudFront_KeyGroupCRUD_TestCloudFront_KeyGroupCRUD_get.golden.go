703e6a49cfcd51d265d8c41c46377e504ea0dcda
sivchari <shibuuuu5@gmail.com>
2026-08-06 13:58:10 +0900

    refactor(test): move cloudfront public key and key group coverage to golden tests

---
+++ test/integration/testdata/cloudfront_test_TestCloudFront_KeyGroupCRUD_TestCloudFront_KeyGroupCRUD_get.golden.go
@@ -0,0 +1,15 @@
+{
+  "ETag": "E4d499465-fb02-484d-8e3e-b6eea256",
+  "KeyGroup": {
+    "Id": "0388392039F3875EE121656F45D0DA77DE61",
+    "KeyGroupConfig": {
+      "Items": [
+        "K0843A049A0BD4"
+      ],
+      "Name": "test-kg-crud-group",
+      "Comment": "CRUD test group"
+    },
+    "LastModifiedTime": "2026-08-06T04:52:59Z"
+  },
+  "ResultMetadata": {}
+}
---
+++ test/integration/testdata/cloudfront_test_TestCloudFront_PublicKeyCRUD_TestCloudFront_PublicKeyCRUD_get.golden.go
@@ -0,0 +1,14 @@
+{
+  "ETag": "Eb0bd4b3c-7434-4d51-a398-7f18bb85",
+  "PublicKey": {
+    "CreatedTime": "2026-08-06T04:52:59Z",
+    "Id": "K595A788EE6A54",
+    "PublicKeyConfig": {
+      "CallerReference": "test-pk-crud",
+      "EncodedKey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvhF44sO4sXzykVryGnoh\naszdr1tZ5qwl54gNmmrACbW+h/lr+ePO4XDSd6G2IZTJfKhotCcIPQ8m87WTHjrS\nPmlUpDa6X+0PyPP6kAS/phvDoADFk9D2u6zLhUc/arwtuPyqP+zts1OHVL6jRz73\nBSKe+CD+YCIrOVLZHFfeG/HwlzzgVOy9ESWVQwr6ERoIk34QRbZP85YxIkfQFUGE\nG5wfuiTHE15IEMKGfYG8xSwAxCIJOoEhLHvXQWUSaO53NIcpLvYVQe6A3RZ1607v\niBYAK1xrfc3GkYVCafMOoNEbfpsgEAh1SkUhtG0LsDVd8I1dtVPCTpMffalOob5D\nKwIDAQAB\n-----END PUBLIC KEY-----\n",
+      "Name": "test-pk-crud-key",
+      "Comment": "CRUD test key"
+    }
+  },
+  "ResultMetadata": {}
+}
